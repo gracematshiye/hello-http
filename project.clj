cd8f5a7fcf50f95f361c4e1a2d09b6560f21e2a5
Grace <gracem@thoughtworks.com>
2017-07-10 12:41:56 +0200

    Basic web application by Ring, first example using clojure

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject hello-http "0.1.0-SNAPSHOT"
+  :description "very basic ring application"
+  ; :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+  				 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :main hello-http.core)
